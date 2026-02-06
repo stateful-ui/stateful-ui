09ce1c152a78517f2f24b860c5e91e0fd64b71bc
Ömer Balyalı <git@omerbalyali.com>
2026-02-06 15:17:34 +0100

    fix(docs): fix title and description

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Stateful CSS",
-  description: "Stateful CSS Documentation",
+  title: "Stateful UI",
+  description: "Stateful UI Documentation",
 };
 
 export default function RootLayout({
